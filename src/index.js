07900fd8bd39d9469454c9210d978c2ec5333f10
Rio-olina <77527798+Rio-olina@users.noreply.github.com>
2021-02-17 00:05:50 +0300

    fix: correct the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-function reverseString(str){
-  str1 = Math.abs(str);
+module.exports = function reverse (str) {
+
+
+ str1 = Math.abs(str);
  str1 = String(str1)
        let a = str1.split('').reverse().join('');
         
 
 let num = Number(a);
 return num }
-
-console.log(reverseString(123))
-console.log(reverseString(-123))
-console.log(reverseString(120))
