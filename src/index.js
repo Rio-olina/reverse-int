50abcdc6f9865079d6047ec5fbb629493a14a28f
Rio-olina <77527798+Rio-olina@users.noreply.github.com>
2021-02-17 00:04:22 +0300

    fix: change the code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-module.exports = function reverse (str) {
-
-
- str = String(str)
-       let a = str.split('').reverse().join('');
+function reverseString(str){
+  str1 = Math.abs(str);
+ str1 = String(str1)
+       let a = str1.split('').reverse().join('');
         
 
 let num = Number(a);
 return num }
+
+console.log(reverseString(123))
+console.log(reverseString(-123))
+console.log(reverseString(120))
