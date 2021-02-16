cb40acfd3fbced22a8ae6dd8334bbb1db8c33b00
Rio-olina <77527798+Rio-olina@users.noreply.github.com>
2021-02-16 23:56:50 +0300

    fix: add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (str) {
+
+
+ str = String(str)
+       let a = str.split('').reverse().join('');
+        
+
+let num = Number(a);
+return num }
